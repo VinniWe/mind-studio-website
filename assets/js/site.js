9297bfa805d8a098fddebaff8d807b5917f9cea5
VinniWe <v.wendler@711rent.com>
2025-09-22 17:11:53 +0200

    UI: responsive header, hero visuals, FormSubmit contact + danke page

---
+++ assets/js/site.js
@@ -0,0 +1,16 @@
+(() => {
+  const toggle = document.querySelector('[data-mobile-toggle]');
+  const menu = document.querySelector('[data-mobile-menu]');
+  if (toggle && menu) {
+    toggle.addEventListener('click', () => {
+      menu.classList.toggle('open');
+    });
+    document.addEventListener('click', (e) => {
+      if (!menu.contains(e.target) && !toggle.contains(e.target)) {
+        menu.classList.remove('open');
+      }
+    });
+  }
+})();
+
+
